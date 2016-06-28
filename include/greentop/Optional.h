c5242095abc4982e026d7fe06890c918a3673a1d
Colin Doig <colind@colind-laptop>
2016-06-28 21:04:32 +1200

    replace BoolJsonMember with Optional<T> for optional primitive types

---
+++ include/greentop/Optional.h
@@ -0,0 +1,37 @@
+#ifndef OPTIONAL_H
+#define OPTIONAL_H
+
+#include "greentop/JsonMember.h"
+
+namespace greentop {
+
+template<typename T>
+class Optional : public JsonMember {
+
+    public:
+        Optional() {
+            valid = false;
+        }
+
+        Optional(T value) : value(value) {
+            valid = true;
+        }
+
+        virtual Json::Value toJson() const {
+            Json::Value json(value);
+            return json;
+        }
+
+        virtual void fromJson(const Json::Value& json);
+
+        operator T () const {
+            return value;
+        }
+
+    private:
+        T value;
+};
+
+}
+
+#endif // OPTIONAL_H
---
+++ src/Optional.cpp
@@ -0,0 +1,11 @@
+#include "greentop/Optional.h"
+
+namespace greentop {
+
+template<>
+void Optional<bool>::fromJson(const Json::Value& json) {
+    value = json.asBool();
+    valid = true;
+}
+
+}
